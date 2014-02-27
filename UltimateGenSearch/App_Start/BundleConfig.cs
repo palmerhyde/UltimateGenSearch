b0e89d69ce075bfde3d46d367704024392ec115f
shaneob <shaneob121@hotmail.com>
2014-02-27 11:16:38 +0000

    initial commit

---
+++ UltimateGenSearch/App_Start/BundleConfig.cs
@@ -0,0 +1,48 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace UltimateGenSearch
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/libs/jquery/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/libs/jquery/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/libs/jquery/jquery.unobtrusive*",
+                        "~/Scripts/libs/jquery/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/libs/modernizer/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+                        "~/Content/bootstrap/bootstrap.min.css",
+                        "~/Content/bootstrap/bootstrap-theme.min.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/libs/bootstrap/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/libs/angular/angular.js*",
+                        "~/Scripts/libs/angular/angular-route.js*",
+                        "~/Scripts/libs/angular/angular-bootstrap.js*",
+                        "~/Scripts/libs/angular/angular-cookies.js*",
+                        "~/Scripts/libs/angular/angular-loader.js*",
+                        "~/Scripts/libs/angular/angular-resource.js*",
+                        "~/Scripts/libs/angular/angular-santize.js*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").IncludeDirectory(
+                        "~/Scripts/app/", "*.js", true));
+        }
+    }
+}
---
+++ UltimateGenSearch/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UltimateGenSearch.MvcApplication" Language="C#" %>
