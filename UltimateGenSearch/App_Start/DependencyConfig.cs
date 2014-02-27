14c7cd16e2e86af78680aad867dbe813b2daa290
shaneob <shaneob121@hotmail.com>
2014-02-27 12:50:35 +0000

    added dummy scrapper

---
+++ UltimateGenSearch/App_Start/DependencyConfig.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using UltimateGenSearch.Services;
+using UltimateGenSearch.Services.Aggregator;
+using UltimateGenSearch.Services.Scrapers;
+
+namespace UltimateGenSearch.App_Start
+{
+    public static class DependencyConfig
+    {
+        internal static IUnityContainer Container { get; private set; }
+
+        internal static void Init(HttpConfiguration config)
+        {
+            Container = new UnityContainer();
+
+            Container.RegisterType<IScraper, DummyScraper>("DummyScraper");
+
+            Container.RegisterType<IAggregator, SimpleAggregator>();
+
+            Container.RegisterType<ISearchService, SearchService>();
+
+            config.DependencyResolver = new Unity.WebApi.UnityDependencyResolver(Container);
+        }
+    }
+}
---
+++ UltimateGenSearch/Controllers/Api/SearchController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using UltimateGenSearch.Models;
+using UltimateGenSearch.Services;
+
+namespace UltimateGenSearch.Controllers.Api
+{
+    public class SearchController : ApiController
+    {
+        private readonly ISearchService _searchService;
+
+        public SearchController(ISearchService searchService)
+        {
+            if (searchService == null)
+                throw new ArgumentNullException("searchService");
+
+            this._searchService = searchService;
+        }
+
+        public HttpResponseMessage Get(Query query)
+        {
+            try
+            {
+                var result = _searchService.Search(query);
+
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+                
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+      
+    }
+}
