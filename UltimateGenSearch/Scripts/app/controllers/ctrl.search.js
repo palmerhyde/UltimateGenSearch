6e0789719ee420105c60226a7bb9304480acf980
shaneob <shaneob121@hotmail.com>
2014-02-27 15:57:03 +0000

    showing search records on home page

---
+++ UltimateGenSearch/Scripts/app/controllers/ctrl.search.js
@@ -0,0 +1,13 @@
+﻿ugsApp.controller('SearchController', ['$scope', '$http', function ($scope, $http) {
+    $scope.records = [];
+    $scope.filterBy = "";
+    $http.get('/Api/Search').then(function (data) {
+        for (var i = 0; i < data.data.length; i++) {
+            $scope.records.push(data.data[i]);
+        }
+    });
+
+    $scope.setFilter = function (_filterBy) {
+        $scope.filterBy = _filterBy;
+    }
+}]);
