a8f191ca5f86b2c1112b834c818aac392f4ef6c4
Claudio Duranti <cduranti@ancestry.ie>
2014-02-27 22:23:41 +0000

    First Login Implementation

---
+++ UltimateGenSearch/Services/Login/Account.cs
@@ -0,0 +1,11 @@
+﻿namespace UltimateGenSearch.Services.Login
+{
+    public class Account
+    {
+        public string Name { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
